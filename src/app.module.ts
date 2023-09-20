6c094054957d050df23242c38e3f7d86973e1689
André Freitas <afelixfreitas@gmail.com>
2023-09-20 00:20:56 -0300

    feat(app): added .env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,22 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'jwt_case',
+      type: process.env.TYPEORM_CONNECTION,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [__dirname + '/**/*.entity{.js,.ts}'],
       synchronize: true,
-    }),
+    } as TypeOrmModuleOptions),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
