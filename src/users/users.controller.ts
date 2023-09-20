9155a26a666ae0280b40df33d99ff994e4e86e3c
André Freitas <afelixfreitas@gmail.com>
2023-09-20 00:36:07 -0300

    feat(users): add basic route implementation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('api/v1/users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findById() {
+    return this.usersService.findById();
+  }
+
+  @Post()
+  create() {
+    return this.usersService.create();
+  }
+
+  @Put()
+  update() {
+    return this.usersService.update();
+  }
+
+  @Delete()
+  delete() {
+    return this.usersService.delete();
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  async findAll() {
+    return null;
+  }
+
+  async findById() {
+    return null;
+  }
+
+  async create() {
+    return null;
+  }
+
+  async update() {
+    return null;
+  }
+
+  async delete() {
+    return null;
+  }
+}
