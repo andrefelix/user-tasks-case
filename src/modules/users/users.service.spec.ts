8d3393771d975680d047b28179e63b2569c52cfd
André Freitas <afelixfreitas@gmail.com>
2023-09-23 07:52:59 -0300

    test(users): add users service findOneOrFail unit test

--- src/modules/users/users.service.spec.ts
+++ src/modules/users/users.service.spec.ts
@@ -3,10 +3,13 @@ import { UsersService } from '../users/users.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { UsersEntity } from './entity/users.entity';
 import { Encryptor } from '../../helpers/encryptor';
-import { mockUserEntity } from '../../helpers/test-helpers';
+import { mockUserDTO, mockUserEntity } from '../../helpers/test-helpers';
+import { Repository } from 'typeorm';
+import { NotFoundException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let usersService: UsersService;
+  let usersEntityRepository: Repository<UsersEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -16,6 +19,7 @@ describe('AuthService', () => {
           provide: getRepositoryToken(UsersEntity),
           useValue: {
             find: jest.fn().mockResolvedValue([mockUserEntity]),
+            findOneOrFail: jest.fn().mockResolvedValue(mockUserDTO),
           },
         },
         {
@@ -26,10 +30,14 @@ describe('AuthService', () => {
     }).compile();
 
     usersService = module.get<UsersService>(UsersService);
+    usersEntityRepository = module.get<Repository<UsersEntity>>(
+      getRepositoryToken(UsersEntity),
+    );
   });
 
   it('should be defined', () => {
     expect(usersService).toBeDefined();
+    expect(usersEntityRepository).toBeDefined();
   });
 
   describe('findAll', () => {
@@ -37,6 +45,27 @@ describe('AuthService', () => {
       const result = await usersService.findAll();
 
       expect(result).toEqual([mockUserEntity]);
+      expect(usersEntityRepository.find).toBeCalledTimes(1);
+    });
+  });
+
+  describe('findOneOrFail', () => {
+    it('should return a founded user', async () => {
+      const result = await usersService.findOneOrFail(mockUserEntity.id);
+
+      expect(result).toEqual(mockUserDTO);
+      expect(usersEntityRepository.findOneOrFail).toBeCalledTimes(1);
+    });
+
+    it('should throw a not found exception error', async () => {
+      jest
+        .spyOn(usersEntityRepository, 'findOneOrFail')
+        .mockRejectedValueOnce(null);
+
+      expect(
+        usersService.findOneOrFail(mockUserEntity.id),
+      ).rejects.toThrowError(NotFoundException);
+      expect(usersEntityRepository.findOneOrFail).toBeCalledTimes(1);
     });
   });
 });
