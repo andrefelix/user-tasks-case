a2a7c9e34bf970545fcac95a9aa612dfb3b0b7ee
André Freitas <afelixfreitas@gmail.com>
2023-09-22 22:46:57 -0300

    chore(auth): add swagger to login route

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -10,14 +10,30 @@ import {
 import { AuthService } from './auth.service';
 import { UserDTO } from '../users/dto/user.dto';
 import { LocalAuthGuard } from './guards/local.guard';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { LoginTokenSwagger } from './swagger/login-token.swagger';
+import { UnauthorizedExceptionSwagger } from 'src/helpers/errors.swagger';
+import { MESSAGE } from 'src/helpers/message';
 
 @Controller('api/v1/auth')
+@ApiTags('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Validação de usuário e senha' })
+  @ApiResponse({
+    status: 200,
+    description: 'Usuário logado com sucesso',
+    type: LoginTokenSwagger,
+  })
+  @ApiResponse({
+    status: 401,
+    description: MESSAGE.invalidAuthentication,
+    type: UnauthorizedExceptionSwagger,
+  })
   login(@Request() req) {
     return this.authService.login(req.user);
   }
