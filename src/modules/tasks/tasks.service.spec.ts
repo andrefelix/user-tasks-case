7b992e8d4ec15c376669e81b5d9ab3c08ddcf280
André Freitas <afelixfreitas@gmail.com>
2023-09-24 10:49:32 -0300

    test(tasks.service): add unit test for create method

--- src/modules/tasks/tasks.service.spec.ts
+++ src/modules/tasks/tasks.service.spec.ts
@@ -1,18 +1,81 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { TasksEntity } from './entity/tasks.entity';
+import { UsersEntity } from '../users/entity/users.entity';
+import { Repository } from 'typeorm';
+import { NotFoundException } from '@nestjs/common';
+
+const userInfo = { id: 'any.id' };
+const newTask = { name: 'new task' };
 
 describe('TasksService', () => {
-  let service: TasksService;
+  let tasksService: TasksService;
+  let tasksRepository: Repository<TasksEntity>;
+  let usersRepository: Repository<UsersEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TasksService],
+      providers: [
+        TasksService,
+        {
+          provide: getRepositoryToken(TasksEntity),
+          useValue: {
+            create: jest.fn().mockReturnValue(newTask),
+            save: jest.fn(),
+          },
+        },
+        {
+          provide: getRepositoryToken(UsersEntity),
+          useValue: {
+            findOneOrFail: jest.fn().mockResolvedValue({}),
+            save: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<TasksService>(TasksService);
+    tasksService = module.get<TasksService>(TasksService);
+    tasksRepository = module.get<Repository<TasksEntity>>(
+      getRepositoryToken(TasksEntity),
+    );
+    usersRepository = module.get<Repository<UsersEntity>>(
+      getRepositoryToken(UsersEntity),
+    );
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(tasksService).toBeDefined();
+    expect(tasksRepository).toBeDefined();
+    expect(usersRepository).toBeDefined();
+  });
+
+  describe('create', () => {
+    const createArgs = {
+      data: { ...newTask },
+      userInfo,
+    };
+
+    it('should create a new task', async () => {
+      const result = await tasksService.create(createArgs);
+
+      expect(result).toEqual(newTask);
+      expect(usersRepository.findOneOrFail).toBeCalledTimes(1);
+      expect(tasksRepository.create).toBeCalledTimes(1);
+      expect(tasksRepository.save).toBeCalledTimes(1);
+      expect(usersRepository.save).toBeCalledTimes(1);
+    });
+
+    it('should throw a not found exception error', () => {
+      jest.spyOn(usersRepository, 'findOneOrFail').mockRejectedValueOnce(null);
+
+      expect(tasksService.create(createArgs)).rejects.toThrowError(
+        NotFoundException,
+      );
+      expect(usersRepository.findOneOrFail).toBeCalledTimes(1);
+      expect(tasksRepository.create).not.toBeCalled();
+      expect(tasksRepository.save).not.toBeCalled();
+      expect(usersRepository.save).not.toBeCalled();
+    });
   });
 });
