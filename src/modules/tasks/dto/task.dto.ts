a881a6107d7ee35a13e440b9203b912b32ad30db
André Freitas <afelixfreitas@gmail.com>
2023-09-25 11:23:19 -0300

    docs(tasks): add swagger api property type

--- src/modules/tasks/dto/task.dto.ts
+++ src/modules/tasks/dto/task.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class TaskDTO {
   @IsNotEmpty({ message: 'A propriedade name deve ser fornecida' })
+  @ApiProperty()
   name: string;
 }
